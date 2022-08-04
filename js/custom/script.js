7c3316cb854a2959c84520c7ebbc6c93dd3f1c04
unknown <sugamanandsahu01@gmail.com>
2022-08-04 14:19:27 +0530

    Adding Nav Toggle.

--- js/custom/script.js
+++ js/custom/script.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
+
+    // HEADER Background on scroll
     window.addEventListener("scroll", function() {
         if(this.scrollY > 100) {
-            $("#header").addClass("active")
-        } else $("#header").removeClass("active")
-    })
+            $("#header").addClass("active");
+        } else $("#header").removeClass("active");
+    });
+
+    // Navbar OPEN and CLOSE on click
+    $(".hamburger").click(function() {
+        console.log("clicked");
+        $("#navbar").addClass("nav-active")
+    });
+
+    $(".nav-close").click(function() {
+        console.log("clicked");
+        $("#navbar").removeClass("nav-active")
+    });
+
+    $(".nav-menu li").click(function() {
+        console.log("clicked");
+        $("#navbar").removeClass("nav-active")
+    });
 })
