bc7eeac63ceeacc281771a0468327f9bed39c556
unknown <sugamanandsahu01@gmail.com>
2022-08-06 00:25:56 +0530

    Adding AOS fnctionality.

--- js/custom/script.js
+++ js/custom/script.js
@@ -7,21 +7,16 @@ $(document).ready(function() {
         } else $("#header").removeClass("active");
     });
 
-    // Navbar OPEN and CLOSE on click
-    $(".hamburger").click(function() {
-        console.log("clicked");
-        $("#navbar").addClass("nav-active")
-    });
-
-    $(".nav-close").click(function() {
-        console.log("clicked");
-        $("#navbar").removeClass("nav-active")
-    });
-
-    $(".nav-menu li a").click(function() {
-        console.log("clicked");
-        $("#navbar").removeClass("nav-active")
-    });
+    // Animation on Scroll
+    (function(){ 
+        AOS.init({
+            offset: 100, // offset (in px) from the original trigger point
+            duration: 900, // values from 0 to 3000, with step 50ms
+            easing: 'ease', // default easing for AOS animations
+            once: false, // whether animation should happen only once - while scrolling down
+            mirror: false, // whether elements should animate out while scrolling past them
+        })
+    }());
 
     // Scroll to SECTIONS 
     $(".scrollTo").click(function() {
@@ -30,14 +25,32 @@ $(document).ready(function() {
             let target = 60; // For seeting top margin
 
             if(window.innerWidth < 756) {
-                target = 80
+                target = 80;
             }
 
             let getOffSet = $(getSection).offset().top - target; // Get top offset
             $("html,body").animate({
                 scrollTop: getOffSet
-            }, 1000)
+            }, 1000);
         }
         return false
-    })
+    });
+
+    // Navbar OPEN and CLOSE on click
+    $(".hamburger").click(function() {
+        console.log("clicked");
+        $("#navbar").addClass("nav-active");
+    });
+
+    $(".nav-close").click(function() {
+        console.log("clicked");
+        $("#navbar").removeClass("nav-active");
+    });
+
+    $(".nav-menu li a").click(function() {
+        console.log("clicked");
+        $("#navbar").removeClass("nav-active");
+    });
+
 })
+
